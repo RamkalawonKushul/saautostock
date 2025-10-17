7ce1baa918cba284f0fdd95810a256f70c51acd1
RamkalawonKushul <akshayramkalawon@gmail.com>
2025-10-17 13:28:07 +0400

    updates

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def aboutus
+  end
+
+  def services
+  end
+
+  def contactus
+  end
+
+  def faq
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  root "pages#index"
+
+  get "aboutus", to: "pages#aboutus"
+  get "services", to: "pages#services"
+  get "faq", to: "pages#faq"
+  get "contactus", to: "pages#contactus"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+end
